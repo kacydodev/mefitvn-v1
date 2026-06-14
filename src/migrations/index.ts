53af59efa79f34fad465aa27b2df6a391261260f
Kacy Do <contact@kacydo.dev>
2026-06-14 18:23:43 +0700

    fix payload issues related to types not found

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20250929_111647 from './20250929_111647';
+import * as migration_20260614_104600 from './20260614_104600';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250929_111647.down,
     name: '20250929_111647',
   },
-]
+  {
+    up: migration_20260614_104600.up,
+    down: migration_20260614_104600.down,
+    name: '20260614_104600'
+  },
+];
